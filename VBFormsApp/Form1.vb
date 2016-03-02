7e881461f4fd5c4125e0dde55ae964be4c398bfe
Marmadook <camins@bell.net>
2016-03-01 22:46:05 -0500

    Add button and picture boxes with images

--- VBFormsApp/Form1.vb
+++ VBFormsApp/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello World!", "MessageBox Title")
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
 End Class
